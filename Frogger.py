32f3b69d9ca590dcc6b33cd2f93edc8dc0345b38
BADuprey <baduprey@uvm.edu>
2025-02-26 16:00:24 -0500

    Added some comments

--- Frogger.py
+++ Frogger.py
@@ -5,18 +5,22 @@ class GameView(arcade.View):
         super().__init__()
         self.background_color = arcade.color.BLACK
 
+    # Resets game
     def reset(self):
         pass
 
+    # Renders everything
     def on_draw(self):
         self.clear()
 
+    # Frame update
     def on_update(self):
         pass
 
+    # Triggers when a key is released
     def on_key_release(self, key, key_modifiers):
 
         pass
-
+    # Triggers when a key is pressed
     def on_key_press(self, key, key_modifiers):
         pass
