d1b98904dd56005e114604506dbb510554f54375
NathanSeashoreHobson <nathanseashorehobson@gmail.com>
2025-02-27 21:54:35 -0500

    Add car/truck textures with example sprites

--- Frogger.py
+++ Frogger.py
@@ -22,12 +22,18 @@ class GameView(arcade.View):
         # -- Load the background -- #
         self.background = self.spritesheet.get_texture(arcade.LBWH(1, 390, 28, 32))
         self.median = self.spritesheet.get_texture(arcade.LBWH(135, 196, SPRITE_SQUARE, SPRITE_SQUARE))
-        self.top_homes = self.spritesheet.get_texture(arcade.LBWH(1, 188, 32, 24))
-        self.top_grass = self.spritesheet.get_texture(arcade.LBWH(35, 188, 8, 24))
-
+        self.top_homes = self.spritesheet.get_texture(arcade.LBWH(1, 188, SPRITE_SQUARE*2, SPRITE_SQUARE*1.5))
+        self.top_grass = self.spritesheet.get_texture(arcade.LBWH(35, 188, SPRITE_SQUARE*.5, SPRITE_SQUARE*1.5))
 
         # -- Load the frog textures -- #
         self.frog_up = self.spritesheet.get_texture(arcade.LBWH(1, 1, SPRITE_SQUARE, SPRITE_SQUARE))
+
+        # -- Load the car textures -- #
+        self.car_1 = self.spritesheet.get_texture(arcade.LBWH(19, 116, SPRITE_SQUARE, SPRITE_SQUARE))
+        self.car_2 = self.spritesheet.get_texture(arcade.LBWH(55, 116, SPRITE_SQUARE, SPRITE_SQUARE))
+        self.car_3 = self.spritesheet.get_texture(arcade.LBWH(1, 116, SPRITE_SQUARE, SPRITE_SQUARE))
+        self.car_4 = self.spritesheet.get_texture(arcade.LBWH(37, 116, SPRITE_SQUARE, SPRITE_SQUARE))
+        self.truck = self.spritesheet.get_texture(arcade.LBWH(73, 116, SPRITE_SQUARE*2, SPRITE_SQUARE))
     
 
     def draw_background(self):
@@ -54,6 +60,18 @@ class GameView(arcade.View):
         self.frog_sprites = arcade.SpriteList()
         self.frog_sprites.append(arcade.Sprite(self.frog_up, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*1.5))
 
+        # Car/Truck examples
+        self.car_1_sprites = arcade.SpriteList()
+        self.car_1_sprites.append(arcade.Sprite(self.car_1, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*2.5))
+        self.car_2_sprites = arcade.SpriteList()
+        self.car_2_sprites.append(arcade.Sprite(self.car_2, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*3.5))
+        self.car_3_sprites = arcade.SpriteList()
+        self.car_3_sprites.append(arcade.Sprite(self.car_3, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*4.5))
+        self.car_4_sprites = arcade.SpriteList()
+        self.car_4_sprites.append(arcade.Sprite(self.car_4, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*5.5))
+        self.truck_sprites = arcade.SpriteList()
+        self.truck_sprites.append(arcade.Sprite(self.truck, scale=SCALE, center_x=WINDOW_WIDTH/2, center_y=SCALED_SQUARE*6.5))
+
 
     # Resets game
     def reset(self):
@@ -66,6 +84,11 @@ class GameView(arcade.View):
         self.draw_background()
 
         self.frog_sprites.draw()
+        self.car_1_sprites.draw()
+        self.car_2_sprites.draw()
+        self.car_3_sprites.draw()
+        self.car_4_sprites.draw()
+        self.truck_sprites.draw()
     
     # Frame update
     def on_update(self, delta_time):
