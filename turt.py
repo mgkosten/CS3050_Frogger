d05e61a2e9c9c01f875116eeaf72b2407b0bf1fe
NathanSeashoreHobson <nathanseashorehobson@gmail.com>
2025-03-27 15:25:13 -0400

    rename turtle class to avoid conflict with built in python class

--- turt.py
+++ turt.py
@@ -1,22 +1,19 @@
 import arcade
 from constants import *
 
-class Turtle:
+class Turt:
     def __init__(self, length, xpos, ypos):
-        # Define Textures dictionary
-        # self.textures = {}
-
         self.speed = OBSTACLE_SPEED
         self.length = length
         self.xpos = xpos
         self.ypos = ypos
         self.sprite = arcade.SpriteList()
-        # self.triple_turtle_sprites = None
-        # self.double_turtle_sprites = None
 
     def load_textures(self, spritesheet):
         '''Loads interactive textures for the turtles
            from the spritesheet into the textures dictionary'''
+        # pylint: disable=unused-variable
+
         x = self.xpos
         y = self.ypos
         turtle_1 = spritesheet.get_texture(
@@ -29,12 +26,12 @@ class Turtle:
             arcade.LBWH(55, 152, SPRITE_SQUARE, SPRITE_SQUARE))
         turtle_disappear_2 = spritesheet.get_texture(
             arcade.LBWH(73, 152, SPRITE_SQUARE, SPRITE_SQUARE))
-        self.sprite.append(arcade.Sprite(turtle_1, SCALE, x, y))
-        x += SCALED_SQUARE
-        for _ in range(self.length - 2):
+
+        # The textures other than 'turtle_2' above are used for swimming animation,
+        # not for drawing the bigger group like with logs
+        for _ in range(self.length):
             self.sprite.append(arcade.Sprite(turtle_2, SCALE, x, y))
             x += SCALED_SQUARE
-        self.sprite.append(arcade.Sprite(turtle_3, SCALE, x, y))
 
     def update(self, delta_time):
         self.xpos += self.speed * delta_time
