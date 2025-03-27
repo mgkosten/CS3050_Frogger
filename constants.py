17b6ee10f9e9b3ac447a3973839adb384f6475a8
NathanSeashoreHobson <nathanseashorehobson@gmail.com>
2025-03-27 15:41:04 -0400

    make pylint happy with constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,11 @@
+'''File to hold constants, import as needed'''
+
+# Scale needs to be an integer so for loops work but change as needed locally
 SCALE = 4
 SPRITE_SQUARE = 16
 SCALED_SQUARE = SPRITE_SQUARE*SCALE
-#for controlling animation speed
 OBSTACLE_SPEED = 3
+
 WINDOW_WIDTH = 28*8*SCALE
 WINDOW_HEIGHT = 32*8*SCALE
 WINDOW_TITLE = "Frogger"
